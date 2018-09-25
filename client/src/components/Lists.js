a8ec1c9fbae38714e4f1201ed30755ed58317d19
Chris Boujoukos <github email address>
2018-09-25 13:03:17 -0400

    configure rack-cors

--- client/src/components/Lists.js
+++ client/src/components/Lists.js
@@ -1,6 +1,26 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class ListsContainer extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      lists: []
+    }
+  }
+
+  componentDidMount(){
+    axios.get('http://localhost:3001/api/v1/lists.json')
+    .then(rsp => {
+      console.log(rsp)
+      this.setState({
+        lists: rsp.data
+      })
+    })
+    .catch(error => console.log(error))
+  }
+
   render() {
     return (
       <div className="Lists-container">
